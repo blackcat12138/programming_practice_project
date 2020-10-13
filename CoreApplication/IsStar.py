6186fe79349f93d4221932cc860504961165fb81
v_shevzliu <v_shevzliu@tencent.com>
2020-10-13 15:18:43 +0800

    加入正确答案

--- CoreApplication/IsStar.py
+++ CoreApplication/IsStar.py
@@ -16,9 +16,9 @@
         ♐           射手座                 11月23日~12月21日
 '''
 
-date = [20, 19, 21, 20, 21, 22, 23, 23, 23, 24, 23, 22]
-star = ["摩羯座", "水瓶座", "双鱼座", "白羊座", "金牛座", "双子座", "巨蟹座", "狮子座", "处女座", "天秤座", "天蝎座", "射手座"]
-signs = ['♑', '♒', '♓', '♈', '♉', '♊', '♋', '♌', '♍', '♎', '♏', '♐']
+sdate = [21, 20, 21, 21, 22, 22, 23, 24, 24, 24, 23, 22]          # 星座判断列表
+conts = ['摩羯座', '水瓶座', '双鱼座', '白羊座', '金牛座', '双子座', '巨蟹座', '狮子座', '处女座', '天秤座', '天蝎座', '射手座', '摩羯座']
+signs = ['♑', '♒', '♓', '♈', '♉', '♊', '♋', '♌', '♍', '♎', '♏', '♐', '♑']
 type = ['务实本分', '作天作地', '安于现状', '积极乐观', '固执内向', '圆滑世故', '多愁善感', '迷之自信', '精明计较', '犹豫不决', '阴暗消极', '放荡不羁', '务实本分']
 
 birth = input('请输入你的出生年月日,格式为：2020-12-12或2000-2-21\n').strip(' ')
@@ -28,14 +28,36 @@ cdate = str(cbir[2])
 
 
 def issign(cmonth, cdate):
-    if int(cdate) < date[int(cmonth) - 1]:  # date[int(02)-1]    --> 19
-        print(star[int(cmonth) - 1])  # star[int(02)-1]    --> 水瓶座
-        print(signs[int(cmonth) - 1])  # signs[int(02)-1]   --> ♒
-        print(type[int(cmonth) - 1])  # type[int(02)-1]    -->作天作地
+    if int(cdate) < sdate[int(cmonth) - 1]:  # date[int(02)-1]    --> 19
+        print(conts[int(cmonth) - 1])        # star[int(02)-1]    --> 水瓶座
+        print(signs[int(cmonth) - 1])        # signs[int(02)-1]   --> ♒
+        print(type[int(cmonth) - 1])         # type[int(02)-1]    -->作天作地
     else:
-        print(star[int(cmonth)])
+        print(conts[int(cmonth)])
         print(signs[int(cmonth)])
         print(type[int(cmonth)])
 
 
 issign(cmonth, cdate)
+
+# sdate = [21, 20, 21, 21, 22, 22, 23, 24, 24, 24, 23, 22]  # 星座判断列表
+# conts = ['摩羯座', '水瓶座', '双鱼座', '白羊座', '金牛座', '双子座', '巨蟹座', '狮子座', '处女座', '天秤座', '天蝎座', '射手座', '摩羯座']
+# signs = ['♑', '♒', '♓', '♈', '♉', '♊', '♋', '♌', '♍', '♎', '♏', '♐', '♑']
+#
+# # 输入生日,输出星座
+# birth = input('请输入你的出生年月日,格式为:2001-02-21\n').strip(' ')
+# cbir = birth.split('-')  # 分割年月日到列表
+# cmonth = str(cbir[1])    # 提取月数据
+# cdate = str(cbir[2])     # 提取日数据
+#
+#
+# def sign(cmonth, cdate):  # 判断星座函数
+#     if int(cdate) < sdate[int(cmonth) - 1]:  # 如果日数据早于对应月列表中对应的日期
+#         print(conts[int(cmonth) - 1])  # 直接输出星座列表对应月对应的星座
+#         print(signs[int(cmonth) - 1])  # 直接输出星座列表对应月对应的星座
+#     else:
+#         print(conts[int(cmonth)])  # 否则输出星座列表下一月对应的星座
+#         print(signs[int(cmonth)])  # 否则输出星座列表下一月对应的星座
+#
+#
+# sign(cmonth, cdate)  # 调用星座判断程序
